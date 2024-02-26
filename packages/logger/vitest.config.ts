c3aedde8fddb52eeedea47f5ac95db0e480ad07c
John Paquette <jmpaquet089@gmail.com>
2024-02-26 09:45:41 -0500

    vitest - fix config

--- packages/logger/vitest.config.ts
+++ packages/logger/vitest.config.ts
@@ -1,5 +1,20 @@
+import { resolve } from 'path';
 import { defineProject, mergeConfig } from 'vitest/config';
 
-import sharedConfig from '../../vite.shared';
+import sharedConfig from '../../vitest.shared';
 
-export default mergeConfig(sharedConfig, defineProject({}));
+export default mergeConfig(
+  sharedConfig,
+  defineProject({
+    build: {
+      assetsDir: '',
+      sourcemap: true,
+      minify: true,
+      lib: {
+        entry: resolve(__dirname, 'src/index.ts'),
+        name: 'Logger',
+        fileName: 'index',
+      },
+    },
+  }),
+);
